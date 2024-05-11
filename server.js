1d3949ce07082e83b47b79c1519ec6fd756a8526
Alex <150617937+ChuddOff@users.noreply.github.com>
2024-05-11 22:55:54 +0300

    alfa 3.0

--- server.js
+++ server.js
@@ -3,21 +3,36 @@ import 'dotenv/config';
 import zamer from './routes/route.js';
 import { MongoClient, ServerApiVersion } from  'mongodb';
 import mongoose, {Model} from 'mongoose';
-import cors from './middlewares/cors.js'
-import logger from './middlewares/logger.js'
+import cors from './middlewares/cors.js';
+import logger from './middlewares/logger.js';
+import {Server} from 'socket.io';
+import {createServer} from 'http';
+// import socketEventRouter from './socket/socket.js';
 
 
 const port = process.env.PORT ?? 4000;
 
 const app = express();
 app.use(express.json());
-app.use(logger)
-app.use(cors)
-app.use("/api/zamer", zamer)
+app.use(logger);
+app.use(cors);
+app.use("/api/zamer", zamer);
+
+
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: `${process.env.API_CLIENT_URL}`,
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    }
+})
+io.on('connection', socket => {
+    socketEventRouter(io, socket)
+})
 
 
 app.get('/', (req, res) => {
-    res.json("text") 
+    res.json("text");
 })
 
 mongoose.connect(
@@ -35,4 +50,4 @@ mongoose.connect(
 ).then(()=>console.log('connected')).catch(e=>console.log(e));
 
 
-app.listen(port)
+server.listen(port)
---
+++ socket/socket.js
@@ -0,0 +1,6 @@
+
+
+
+// ​​export default function socketEventRouter(io, socket) {
+    
+// }
